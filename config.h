50b28561b69f2b4937b628c5b0ce90989b343d67
Ali Gholami Rudi <ali@rudi.ir>
2011-09-18 22:43:21 +0430

    config.h: comments for account and filter struct fields

--- config.h
+++ config.h
@@ -5,22 +5,22 @@
 #define DPRINT(msg, len)
 
 struct account {
-	char *server;
-	char *port;
-	char *user;
-	char *pass;
-	char *uidl;
-	char *cert;
-	int del;
-	int nopipe;
+	char *server;		/* server address */
+	char *port;		/* server port */
+	char *user;		/* username */
+	char *pass;		/* password */
+	char *uidl;		/* path of uidl file; disables uidl if NULL */
+	char *cert;		/* path of server certificate pem file */
+	int del;		/* delete messages after retrieving */
+	int nopipe;		/* no outstanding commands */
 } accounts [] = {
 	{"server", "port", "username", "password"},
 };
 
 struct filter {
-	char *hdr;
-	char *val;
-	char *dst;
+	char *hdr;		/* header name */
+	char *val;		/* pattern to match */
+	char *dst;		/* destination mbox file */
 } filters[] = {
 	{"HDR:", "KEY", FOLDER "dst"},
 };
